7741f7c35a7cafa0abc4a86608dc9489763a6331
ASHUTOSH SHUKLA <ashutoshshukla734.as@gmail.com>
2024-05-29 11:55:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+<script>
+  // Add some dynamic effects
+  const readme = document.getElementById('readme');
+  const codeBlocks = readme.querySelectorAll('code');
 
+  function animateText(element) {
+    let text = element.textContent;
+    let speed = 50; // Adjust speed as desired
+    let i = 0;
+
+    element.textContent = '';
+
+    function typeWriter() {
+      if (i < text.length) {
+        element.textContent += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+
+    typeWriter();
+  }
+
+  codeBlocks.forEach(codeBlock => {
+    animateText(codeBlock);
+  });
+</script>
